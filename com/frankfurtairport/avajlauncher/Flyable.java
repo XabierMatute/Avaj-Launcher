6aa1f64640fab807361687879aa56fcb94a14209
xmatute- <xmatute-@student.42urduliz.com>
2025-02-03 20:02:19 +0100

    Flyabla

---
+++ com/frankfurtairport/avajlauncher/Flyable.java
@@ -0,0 +1,27 @@
+package com.frankfurtairport.avajlauncher;
+
+/**
+ * Abstract class representing a flyable object.
+ */
+abstract class Flyable
+{
+    /**
+     * The weather tower to which the flyable object is registered.
+     */
+    protected WeatherTower weatherTower;
+
+    /**
+     * Updates the conditions of the flyable object.
+     */
+    public abstract void updateConditions();
+
+    /**
+     * Registers the flyable object with a weather tower.
+     * @param weatherTower The weather tower to register with.
+     */
+    public void registerTower(WeatherTower weatherTower)
+    {
+        this.weatherTower = weatherTower;
+        weatherTower.register(this);
+    }
+}
